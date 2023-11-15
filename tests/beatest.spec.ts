11babbb90caac6a126f331872884e4a3e4b27f78
Beatriz Ferreira <biiasinha_@hotmail.com>
2023-11-15 10:29:46 +0000

    spec

---
+++ tests/beatest.spec.ts
@@ -0,0 +1,13 @@
+import {test,expect} from '@playwright/test';
+
+//test.describe ('Open google', ({page})  => {
+ //   await page.goto('https://www.google.com');
+//    await expect(page).toHaveTitle(/Google/);
+ // });
+
+test.describe('Open Google', () => {
+  test('Check its open', async ({ page }) => {
+    await page.goto('https://www.google.com');
+    await expect(page).toHaveTitle("Google");
+    });
+  });
--- tests/beatest.ts
+++
@@ -1,6 +0,0 @@
-import {test,expect} from '@playwright/test';
-
-test ('Open google', async ({page})  => {
-    await page.goto('https://www.google.com');
-    await expect(page).toHaveTitle(/Google/);
-  });
